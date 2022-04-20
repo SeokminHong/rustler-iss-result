d86192efe33f93aec515d008cf5192259fe1eb95
Seokmin Hong <ghdtjrald240@gmail.com>
2022-04-20 10:45:47 +0900

    Implement the case for compile error

--- native/test/src/lib.rs
+++ native/test/src/lib.rs
@@ -1,3 +1,15 @@
+#[derive(Debug)]
+pub enum MyError {
+    MyError,
+}
+
+type Result<T> = std::result::Result<T, MyError>;
+
+#[derive(rustler::NifMap)]
+struct TestStruct {
+    a: i32,
+}
+
 #[rustler::nif]
 fn add(a: i64, b: i64) -> i64 {
     a + b
